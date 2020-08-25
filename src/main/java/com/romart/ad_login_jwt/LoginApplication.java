31bcca1ee1eabacb0a429297822b60c79abefd7d
robin.martini <r.martini.robin@gmail.com>
2020-08-25 11:32:55 +0200

    renaming main app

--- src/main/java/com/romart/ad_login_jwt/LoginApplication.java
+++ src/main/java/com/romart/ad_login_jwt/LoginApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LdapDemoApplication {
+public class LoginApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LdapDemoApplication.class, args);
+		SpringApplication.run(LoginApplication.class, args);
 	}
 
 
